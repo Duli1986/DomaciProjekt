7ff0bcc73a8f823e4606cedf454126c039af3baf
Duli1986 <dulovec.martin@gmail.com>
2020-02-07 22:53:28 +0100

    add dto

---
+++ src/main/java/com/example/demo/dto/Customer.java
@@ -0,0 +1,65 @@
+package com.example.demo.dto;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "typesex")
+    private String typesex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTypesex() {
+        return typesex;
+    }
+
+    public void setTypesex(String typesex) {
+        this.typesex = typesex;
+    }
+}
