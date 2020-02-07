400bdc30b61be1a56f53db1b5916d7d589bffccd
Duli1986 <dulovec.martin@gmail.com>
2020-02-07 23:14:09 +0100

    add interface for Customer

--- src/main/java/com/example/demo/services/CustomerServices.java
+++ src/main/java/com/example/demo/services/CustomerServices.java
@@ -1,4 +1,26 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.Customer;
+
+import java.util.List;
+
 public interface CustomerServices {
+
+    Customer findCustomerById(Long id);
+
+    List<Customer> findCustomer();
+
+    Customer saveCustomer(Customer customer);
+
+    void deleteCustomer(Long id);
+
+    void addCustomer(Long id);
+
+    List<Customer> findCustomerByName(String name);
+
+    List<Customer> findCustomerBySurname(String surname);
+
+    List<Customer> findCustomerByAge(Integer age);
+
+    List<Customer> findCustomerByTypeSex(String typeSex);
 }
