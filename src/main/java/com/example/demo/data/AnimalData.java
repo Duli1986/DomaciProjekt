ff8b0a094d5281e79a1c0ad6815d3891cb15da24
Duli1986 <dulovec.martin@gmail.com>
2020-02-08 13:47:04 +0100

    refactor from Customer to Animal

--- src/main/java/com/example/demo/data/AnimalData.java
+++ src/main/java/com/example/demo/data/AnimalData.java
@@ -1,4 +1,42 @@
 package com.example.demo.data;
 
-public class AnimalData {
+import com.example.demo.dto.Animal;
+import com.example.demo.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class AnimalData implements CommandLineRunner {
+
+    public AnimalData(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    @Autowired
+    private AnimalRepository animalRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(" Loading Animal Data");
+
+    Animal c1 = new Animal();
+    c1.setName("Martin");
+    c1.setAge(3);
+    c1.setTypesex("male");
+    animalRepository.save(c1);
+
+    Animal c2 = new Animal();
+    c2.setName("Karel");
+    c2.setAge(8);
+    c2.setTypesex("male");
+    animalRepository.save(c2);
+
+    Animal c3 = new Animal();
+    c3.setName("Gorila");
+    c3.setAge(10);
+    c3.setTypesex("female");
+    animalRepository.save(c3);
+
+    System.out.println("Animals Saved: " + animalRepository.count());
+    }
 }
